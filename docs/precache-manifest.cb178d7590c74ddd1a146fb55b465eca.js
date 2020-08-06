c9f33afb106abfadcfd8c296b1972d62a124a622
yeison bastidas <yeison.b.b@hotmail.com>
2020-08-05 19:09:47 -0500

    nuevo archivo docs

--- docs/precache-manifest.cb178d7590c74ddd1a146fb55b465eca.js
+++ docs/precache-manifest.cb178d7590c74ddd1a146fb55b465eca.js
@@ -1,26 +1,26 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "127d8f5bb861a9a990eed6b1652301d9",
-    "url": "/index.html"
+    "url": "./index.html"
   },
   {
     "revision": "0a91f036d308ce011df7",
-    "url": "/static/css/main.f670d3d9.chunk.css"
+    "url": "./static/css/main.f670d3d9.chunk.css"
   },
   {
     "revision": "95561dca39e7d9a47140",
-    "url": "/static/js/2.839bf086.chunk.js"
+    "url": "./static/js/2.839bf086.chunk.js"
   },
   {
     "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
-    "url": "/static/js/2.839bf086.chunk.js.LICENSE.txt"
+    "url": "./static/js/2.839bf086.chunk.js.LICENSE.txt"
   },
   {
     "revision": "0a91f036d308ce011df7",
-    "url": "/static/js/main.14671c67.chunk.js"
+    "url": "./static/js/main.14671c67.chunk.js"
   },
   {
     "revision": "daee87f482498cdf724c",
-    "url": "/static/js/runtime-main.29242b1d.js"
+    "url": "./static/js/runtime-main.29242b1d.js"
   }
 ]);
